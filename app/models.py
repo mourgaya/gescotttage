1aecfd938bf1429596920ea623ce855a48f7097b
mourgaya <eric.mourgaya@gmail.com>
2017-07-06 23:50:12 +0200

    modification  schema

--- app/models.py
+++ app/models.py
@@ -1,19 +1,15 @@
 from app import db
 
-# 1 locataire a 0,1 ou plusieurs gites
-# 1 locataire a 0,1 ou plusieurs reservation
-class Locataires(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class Client(db.Model):
+    email = db.Column(db.String(120), primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
-    surname = db.Column(db.String(64), index=True, unique=True)
-    telephone = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    commentaire = db.Column(db.String(240), index=True, unique=True)
-    addresse = db.Column(db.String(240), index=True, unique=True)
+    surname = db.Column(db.String(64), unique=True)
+    telephone = db.Column(db.String(64), unique=True)
+    adresse = db.Column(db.String(240),  unique=True)
     reference = db.Column(db.String(64), index=True, unique=True)	
-    ville = db.Column(db.String(64), index=True, unique=True)	
-    pays = db.Column(db.String(64), index=True, unique=True)	
-    codepostal = db.Column(db.Integer, primary_key=True)
+    ville = db.Column(db.String(64), unique=True)	
+    pays = db.Column(db.String(64), unique=True)	
+    codepostal = db.Column(db.Integer)
 
     def __init__(self, name, surname,telephone,email,commentaire=None,addresse=None,reference=None,ville=none,pays=none):
         self.email = email
@@ -24,79 +20,63 @@ class Locataires(db.Model):
     def __repr__(self):
         return '<Locataires %r>' % (self.name)
 
-#1 gite a 1 exploitation journaliere
-#1 gite a 1 reservation ?
+class Reservation(db.Model):
+    email = db.Column(db.String(120), primary_key=True)
+    gites = db.relationship('Gites', backref='gites', lazy='dynamic')
+    nbadultes = db.Column(db.Integer, index=True, unique=True)
+    datedebut = db.Column(db.DateTime, index=True, unique=True)	
+    datefindebut = db.Column(db.DateTime, index=True, unique=True)	
+    nbenfant = db.Column(db.Integer, index=True, unique=True)
+    cout = db.Column(db.Integer, index=True, unique=True)
+    montantaccompte = db.Column(db.Integer, index=True, unique=True)# 0  accompte non payé
+    indicateurpaiment = db.Column(db.Integer, index=True, unique=True)# 1 ok
+
+#(resa1,chaumiere,extra)
+class Planninggite(db.Model):
+    Reservation=
+    Gite=
+    extra=
+
+#(chaumiere,"http://photo","http://plan","http://agenda",7,1,2)
 class Gites(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-    photos = db.Column(db.String(120), index=True, unique=True)
-    plan = db.Column(db.String(120), index=True, unique=True)
-    contrat = db.Column(db.String(120), index=True, unique=True)
-    addresse = db.Column(db.String(120), index=True, unique=True)
-    capacity = db.Column(db.Integer, index=True, unique=True)
-    latittude = db.Column(db.float, index=True, unique=True)
-    longitude = db.Column(db.float, index=True, unique=True)
-    prixperdaybas = db.Column(db.Integer, index=True, unique=True)
-    prixperdaysweek = db.Column(db.Integer, index=True, unique=True)
-    prixper3daysweek = db.Column(db.Integer, index=True, unique=True)
-    prixperdayhaut = db.Column(db.Integer, index=True, unique=True)
+    name = db.Column(db.String(64),  primary_key=True)
+    photos_url = db.Column(db.String(120), unique=True)
+    plan_url = db.Column(db.String(120),  unique=True)
+    agenda_url = db.Column(db.String(120),  unique=True)
+    capacity = db.Column(db.Integer)
+    nbsdb = db.Column(db.Integer)
+    adresse = db.Column(db.String(120), index=True, unique=True)
+    litsup = db.Column(db.integer)
+    
+
+#("chaumiere",90,150,120,110,100)
+class Tarif(db.Model)
+    gite = db.Column(db.String(64), primary_key=True)
+    prixmoyend = db.Column(db.Integer)
+    prixhautd = db.Column(db.Integer)
+    prixweek2d = db.Column(db.Integer)
+    prixweek3d = db.Column(db.Integer)
+    prixweek4d = db.Column(db.Integer)
 
     def __init__(self,name,photos=None,plan=None,contrat=None,addresse=None,capacity=None,prixperdaysweek=None,prixperdayweek=None,prixperdayshaut=None)
 	
     def __repr__(self):
         return '<Gites %r>' % (self.name)
-	
-# 1 reservation  a 1 locataire
-# 1 reservation a 1 ou plusieurs gites
-# 1 reservation a 1 ou plusieurs paiement
-# 1 reservation a 1 ou plusieurs extras
-# 1 reservation a 1 ou plusieurs  contrats 
-class Reservation(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    numero = db.Column(db.Integer, index=True, unique=True)
-    nbadultes = db.Column(db.Integer, index=True, unique=True)
-    nbenfant = db.Column(db.Integer, index=True, unique=True)
-    nblitdoubles = db.Column(db.Integer, index=True, unique=True)
-    nblitsimple = db.Column(db.Integer, index=True, unique=True)
-    locataire = db.relationship('Locataire', backref='locataires', lazy='dynamic')
-    gites = db.relationship('Gites', backref='gites', lazy='dynamic')
-    cout = db.Column(db.Integer, index=True, unique=True)
-    paiement = deb.relationship()
-    events = deb.relationship()
-    
-# 1 paiement correspond a 1 reservation
-class Paiement(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    numero = db.Column(db.Integer, index=True, unique=True)
-    createdate = db.Column(db.DateTime, index=True, unique=True)	
-    montant = db.Column(db.Float, index=True, unique=True)
-    deposit = db.Column(db.Float, index=True, unique=True)
-    solde = db.Column(db.Float, index=True, unique=True)	
-    depositpaid= db.Column(db.Boolean, index=True, unique=True)	
-    soldepaid= db.Column(db.Boolean, index=True, unique=True)	
-    extrapaid= db.Column(db.Boolean, index=True, unique=True)	
-    
 
+#("eric.mourgaya@gmail.com","chaumiere",01,1)
 class Extra(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    quantite = db.Column(db.Integer, primary_key=True)
-    prixunitaire = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), primary_key=True)
+    gites = db.relationship('Gites', backref='gites', lazy='dynamic')
+    TypeExtra = db.relationship('Gites', backref='gites', lazy='dynamic')
+    quantite = db.Column(db.Integer)
 
 
-# una exploitation journaliere  est associé a 1 gite
-class exploitejournalier(db.Model)
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-    gites = deb.relationship()
-    date = db.Column(db.DateTime, index=True, unique=True)
-    ppd = db.Column(db.integer, index=True, unique=True)
-    availability = db.Column(db.Boolean, index=True, unique=True)
-
-# un event est associé a  une ou plusieur  exploitation  journaliere
-class event(db.model):
-    id = db.Column(db.Integer, primary_key=True)
-    debutdb.Column(db.DateTime,index=True,Unique=True)
-    nbjours = db.Column(db.Integer, index=True, unique=True)
-    	
+#(01,"menage",54)#
+#(02,"draps",20)#
+#(03,"lit bebe",0)#
+#(04,"rangement lavevaille",10)#
+class Typeextra(db.Model):
+   CodeExtra = db.Column(db.String(120), primary_key=True)
+   libelle = db.Column(db.String(240),  unique=True)
+   cout = db.Column(db.Integer)
 
